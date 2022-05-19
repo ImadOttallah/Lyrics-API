8a420b9a9e272540fc5839823a66d04bf9e9eed0
Imad Ottallah <imadotta@gmail.com>
2022-05-19 13:20:58 -0500

    added form to the call lyrics need to modularize it

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,48 @@
 // USE WITH FIREBASE AUTH
 // import checkLoginStatus from './helpers/auth';
+import axios from 'axios';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
-  console.warn('YOU ARE UP AND RUNNING!');
+const renderToDom = (divId, textToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToRender;
+};
+const getLyrics = (artist, song) => new Promise((resolve, reject) => {
+  axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
 
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+const lyricsOnDom = (artist, song) => {
+  getLyrics(artist, song).then((response) => {
+    document.querySelector('#lyrics-container').innerHTML = response.lyrics;
+  });
+};
+const searchLyrics = () => {
+  let domString = '';
+  domString = `<form>
+  <div class="mb-3">
+    <label for="artist" class="form-label">Artist</label>
+    <input type="text" class="form-control" id="artist" required>
+  </div>
+  <div class="mb-3">
+    <label for="song" class="form-label">Song</label>
+    <input type="text" class="form-control" id="song" required>
+  </div>
+  <button type="submit" class="btn btn-primary" id="song-btn">Submit</button>
+</form>`;
+  renderToDom('#app', domString);
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputArtist = document.querySelector('#artist').value;
+    const inputSong = document.querySelector('#song').value;
+    lyricsOnDom(inputArtist, inputSong);
+  });
 };
 
-init();
+const startApp = () => {
+  // lyricsOnDom();
+  searchLyrics();
+};
+startApp();
